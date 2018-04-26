19bdc79ee2dc4af3f35e2a722511b3a428f3344f
RstlssVsn <victoria_95m@mail.ru>
2018-04-26 09:08:12 +0300

    Create arxiv_iee_pars_v2.py

---
+++ arxiv_iee_pars_v2.py
@@ -0,0 +1,49 @@
+from urllib.request import urlopen
+
+from bs4 import BeautifulSoup
+import urllib.request
+import re
+import json
+
+url = 'http://export.arxiv.org/api/query?search_query=system+dynamics&max_results=3'
+with urlopen(url) as request:
+     data = request.read()
+number = 1
+from xml.etree import ElementTree
+root = ElementTree.fromstring(data)
+
+for entry in root.findall('{http://www.w3.org/2005/Atom}entry'):
+             title = entry.findall('{http://www.w3.org/2005/Atom}title')[0].text
+             summary = entry.findall('{http://www.w3.org/2005/Atom}summary')[0].text
+             published_date = entry.findall('{http://www.w3.org/2005/Atom}published')[0].text
+             authors = [x[0].text for x in entry.findall('{http://www.w3.org/2005/Atom}author')]
+             print(str(number), '[Title]: '+ title.replace('\n',' ') + '\n', ' [Published_date]: ' + published_date + '\n', ' [Authors]: ' + str(authors).replace('[','').replace(']','') + '\n', ' [Abstract]: ' + summary.replace('\n',' '), '\n')
+             number += 1   
+
+url_1 = 'http://ieeexplore.ieee.org/xpl/mostRecentIssue.jsp?punumber=4410506'
+response = urllib.request.urlopen(url_1)
+soup = BeautifulSoup(response.read().decode('utf-8'), "html.parser")
+
+for element in soup.find_all('a', href=re.compile("stamp\\.jsp")):
+    page_url = 'http://ieeexplore.ieee.org' + element.get('href')
+    page_response = urllib.request.urlopen(page_url)
+    page_soup = BeautifulSoup(page_response.read().decode('utf-8'), "html.parser")
+    json_re = re.compile("global\\.document\\.metadata\\s*=\\s*(.+);")
+    if number < 5:
+                  for page_element in page_soup.find_all('script', string=re.compile("global\\.document\\.metadata")):
+                      match = json_re.search(page_element.string)
+                      data_1 = json.loads(match.group(1))
+                      doi = data_1['doi']
+                      authors = data_1['authors']
+                      a1 = str(authors).find('name') + 6
+                      a2 = str(authors).find('affiliation') - 3
+                      author_name = str(authors)[a1:a2]
+                      abstract = data_1['abstract']
+                      title_1 = data_1['title']
+                      dateOfInsertion = data_1['dateOfInsertion']
+                      print(str(number), '[Title]: ' + title.replace('\n',' '))
+                      print('  [Published_date]: ' + published_date)
+                      print('  [Authors]: ' + author_name)
+                      print('  [Abstract]: ' + abstract.replace('\n',' '))
+
+    number += 1   
